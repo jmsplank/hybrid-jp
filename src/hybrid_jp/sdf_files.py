e6835ad27e0e20f44534b59fd219d66728178dab
James Plank <plankjames@gmail.com>
2023-03-28 18:07:57 +0100

    fix: refactor fn's from quickload into sdf_files

--- src/hybrid_jp/sdf_files.py
+++ src/hybrid_jp/sdf_files.py
@@ -1,10 +1,110 @@
 """Functions that interact with sdf_helper objects."""
+from pathlib import Path
 from typing import cast
 
 import numpy as np
+import pandas as pd
+import sdf_helper as sh
+from sdf import BlockList
 
-from .constants import VAR_BX, VAR_BY, VAR_BZ, VAR_GRID, VAR_GRID_MID
 from .dtypes import Grid, Mag
+
+
+def load(path_to_sdf: Path | str) -> BlockList:
+    """Load sdf file into sdf_helper object.
+
+    - if str is passed, it is converted to Path
+
+    Args:
+        path_to_sdf (Path | str): path to sdf file
+
+    Raises:
+        FileNotFoundError: file not found
+        ValueError: file is not an sdf
+
+    Returns:
+        sdf_helper_internal: sdf data object
+    """
+    if not isinstance(path_to_sdf, Path):
+        path_to_sdf = Path(path_to_sdf)
+    if not path_to_sdf.exists():
+        raise FileNotFoundError(f"File not found: {path_to_sdf}")
+    if not path_to_sdf.suffix == ".sdf":
+        raise ValueError(f"File is not an sdf: {path_to_sdf}")
+
+    data = sh.getdata(str(path_to_sdf), verbose=False)
+    return data
+
+
+def extract_vars(data, vars_list: list[str]) -> dict[str, np.ndarray]:
+    """Get vars from `vars_list` out of sdf data object.
+
+    Args:
+        data (sdf_helper_internal): sdf data object
+        vars_list (list[str]): list of variable names
+
+    Returns:
+        dict[str, np.ndarray]: dict of np arrays, keys are var names
+    """
+    out = {}
+    grid_name = "Grid_Grid_mid"
+    for var in vars_list:
+        dat = getattr(data, var).data
+        if var == grid_name:
+            dat = dat[0]  # only care about x
+        out[var] = dat
+
+    return out
+
+
+def reduce_to_1d(data: dict[str, np.ndarray], index: int = 80) -> dict[str, np.ndarray]:
+    """Take y=`index` from each key in dict of 2d np arrays.
+
+    Args:
+        data (dict[str, np.ndarray]): dict of 2d np arrays
+        index (int, optional): y=`index`. Defaults to 80.
+
+    Raises:
+        ValueError: Raised when any key in dict has dimension != 2
+
+    Returns:
+        dict[str, np.ndarray]: dict of 1d np arrays
+    """
+    out = {}
+    for k, v in data.items():
+        print(k, type(v), v.shape)
+        if len(v.shape) == 2:
+            out[k] = v[:, index]
+        elif len(v.shape) == 1:
+            out[k] = v
+        else:
+            raise ValueError(
+                f"{k} has too many/not enough dimensions. Has shape {v.shape}"
+            )
+
+    return out
+
+
+def data_to_df(data: dict[str, np.ndarray]) -> pd.DataFrame:
+    """Turn dict into dataframe.
+
+    Args:
+        data (dict[str, np.ndarray]): dict
+
+    Returns:
+        pd.DataFrame: dataframe
+    """
+    return pd.DataFrame(data)
+
+
+def save_csv(path: Path, data: pd.DataFrame) -> None:
+    """Save dataframe as csv at `path`.
+
+    Args:
+        path (Path): Path (inc. filename) to output
+        data (pd.DataFrame): dataframe to write
+    """
+    data.to_csv(path, index=False)
 
 
 def list_variables(
@@ -83,8 +183,10 @@ def get_grid(data, mid: bool = False) -> Grid:
             y=array([0.00000000e+00, 2.50000000e-05, 5.00000000e-05, ...,
                 5.00000000e-02, 5.00250000e-02, 5.00500000e-02]))
     """
-    attr_name = VAR_GRID if not mid else VAR_GRID_MID
-    attr = getattr(data, attr_name).data
+    if mid:
+        attr = data.Grid_Grid_mid.data
+    else:
+        attr = data.Grid_Grid.data
     return Grid(x=attr[0], y=attr[1])
 
 
@@ -104,7 +206,8 @@ def get_mag(data) -> Mag:
             y=array([[ 0.00000000e+00,  1.00000000e-16,  2.00000000e-16, ...,]],
             z=array([[ 0.00000000e+00,  1.00000000e-16,  2.00000000e-16, ...,]]))
     """
-    components = []
-    for i in [VAR_BX, VAR_BY, VAR_BZ]:
-        components.append(getattr(data, i).data)
-    return Mag._make(components)
+    return Mag(
+        bx=data.Magnetic_Field_Bx.data,
+        by=data.Magnetic_Field_By.data,
+        bz=data.Magnetic_Field_Bz.data,
+    )
