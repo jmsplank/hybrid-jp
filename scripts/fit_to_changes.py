77b0795849e62528d5c29dc5a25caf9175571a7f
James Plank <plankjames@gmail.com>
2023-04-13 15:03:57 +0100

    feat: averaging and remapping
    - refactor moving average plotting to separate function
    - use MovingAverageResult to contain result of moving average
    - function remap_to_original_t that interpolates shorter
        (len-width+1) linterp to original length of t. Matches with
        original sdf files.

--- scripts/fit_to_changes.py
+++ scripts/fit_to_changes.py
@@ -1,6 +1,6 @@
 """Perform regressions on the shock changes."""
 from pathlib import Path
-from typing import cast
+from typing import NamedTuple, cast
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,6 +8,7 @@ import pandas as pd
 from phdhelper.helpers import override_mpl
 from phdhelper.helpers.COLOURS import blue, green, red
 from rich import print as rprint
+from scipy.interpolate import interp1d
 from scipy.stats import linregress
 
 
@@ -191,41 +192,172 @@ def interpolate_to_midpoints(arr: np.ndarray, width: int) -> np.ndarray:
     return np.linspace(arr[0], arr[-1], len(arr) - width + 1)
 
 
-def moving_avg(df: pd.DataFrame) -> None:
+class MovingAverageResult(NamedTuple):
+    """Holds the results of a moving average.
+
+    Attributes:
+        t (np.ndarray): time.
+        start_STR (np.ndarray): x coordinate of the start of the STR.
+        end_STR (np.ndarray): x coordinate of the end of the STR.
+        shock (np.ndarray): x coordinate of the shock.
+    """
+
+    t: np.ndarray
+    start_STR: np.ndarray
+    end_STR: np.ndarray
+    shock: np.ndarray
+
+    def to_df(self) -> pd.DataFrame:
+        """Convert to a dataframe.
+
+        Returns:
+            pd.DataFrame: dataframe containing t, start_STR, end_STR, shock.
+        """
+        return pd.DataFrame(
+            dict(
+                t=self.t,
+                start_STR=self.start_STR,
+                end_STR=self.end_STR,
+                shock=self.shock,
+            )
+        )
+
+    def save(self, path: Path) -> None:
+        """Save to csv by converting to dataframe.
+
+        Args:
+            path (Path): path to save to.
+        """
+        self.to_df().to_csv(path, index=False)
+
+    def _t_index(self, t: float) -> int:
+        """Get the index of t in self.t.
+
+        Args:
+            t (float): time.
+
+        Returns:
+            int: index of t in self.t.
+        """
+        return int(np.argmin(np.abs(self.t - t)))
+
+    def get_irow(self, index: int) -> tuple[float, tuple[float, float], float]:
+        """Get the row at index.
+
+        Args:
+            index (int): index of row.
+
+        Returns:
+            tuple[float, tuple[float, float], float]: [t, (start_STR, end_STR), shock]
+        """
+        return (
+            self.t[index],
+            (self.start_STR[index], self.end_STR[index]),
+            self.shock[index],
+        )
+
+    def get_row(self, t: float) -> tuple[tuple[float, float], float]:
+        """Get the row at time t.
+
+        Args:
+            t (float): time.
+
+        Returns:
+            tuple[float, float, float]: [start_STR, end_STR, shock]
+        """
+        index = self._t_index(t)
+        return self.get_irow(index)[1:]
+
+
+def moving_avg(df: pd.DataFrame) -> MovingAverageResult:
+    """Perform a moving average on the shock changes.
+
+    df should have the following columns:
+    - `shock`
+    - `change_0`
+    - `change_1`
+    - `change_2`
+
+    - Moving average performed on each column minus the linear regression of that column.
+    - returned array is of length len(df) - width + 1 where width is the width of the moving average.
+    - start coordinate of STR is minimum of the change points
+    - end coordinate of STR is mean of change_1 and change_2
+    - shock coordinate is maximum of shock and change_0
+
+    Args:
+        df (pd.DataFrame): dataframe containing shock changes.
+
+    Returns:
+        MovingAverageResult: moving average of shock changes.
+
+    """
     # Get the slope of each column in df
     lines = linear_regressions(df)
     width = 8  # width of moving average of form 2^n where n is an integer
 
-    fig, ax = plt.subplots()
-
     changes = list(lines.keys())
-    colours = iter(["k", red, green, blue])
-    for change in changes:
-        col = next(colours)
+    restored_slope = np.empty((len(changes), len(df.index) - width + 1))  # (x, t)
+    for i, change in enumerate(changes):
         slope = np.array(lines[change]["slope"] * df.index + lines[change]["intercept"])
         subtracted_slope = np.array(df[change] - slope)
         width_mov_avg = mov_avg(subtracted_slope, width)
-        restored_slope = width_mov_avg + interpolate_to_midpoints(slope, width)
+        restored_slope[i, :] = width_mov_avg + interpolate_to_midpoints(slope, width)
 
-        # plot change as scatterplot of hollow circles
-        ax.scatter(df.index, df[change], color=col, marker="o", facecolors="none")  # type: ignore
-        # plot moving average of change as solid line
-        ax.plot(
-            interpolate_to_midpoints(np.array(df.index), width),
-            restored_slope,
-            color=col,
-            linestyle="-",
-        )
-        ax.plot(
-            interpolate_to_midpoints(np.array(df.index), width),
-            mov_avg(np.array(df[change]), width),
-            color=col,
-            linestyle="--",
-        )
+    new_t = interpolate_to_midpoints(np.array(df.index), width)
+    str_start = np.min(restored_slope, axis=0)
+    str_end = np.mean(restored_slope[2:, :], axis=0)
+    shock = np.max(restored_slope[:2, :], axis=0)
+
+    return MovingAverageResult(new_t, str_start, str_end, shock)
+
+
+def plot_mov_avg(moving_avg: MovingAverageResult):
+    """Plot the moving average.
+
+    Args:
+        moving_avg (MovingAverageResult): moving average to plot.
+    """
+    fig, ax = plt.subplots()
+    ax.plot(moving_avg.t, moving_avg.shock, color="k", linestyle="-", label="Shock")
+    ax.fill_between(
+        moving_avg.t,
+        moving_avg.start_STR,  # type: ignore
+        moving_avg.end_STR,  # type: ignore
+        color=green,
+        alpha=0.5,
+        label="STR",
+    )
+
+    ax.set_ylabel("x (m)")
+    ax.set_xlabel("t (s)")
 
     plt.legend()
     plt.tight_layout()
     plt.show()
+
+
+def remap_to_original_t(
+    original_t: np.ndarray, changes: MovingAverageResult
+) -> MovingAverageResult:
+    """Remap the changes to the original time.
+
+    Note:
+        since bounds of original_t are necessaril outside of changes.t, the values are extrapolated.
+
+    Args:
+        original_t (np.ndarray): original time.
+        changes (MovingAverageResult): changes to remap.
+
+    Returns:
+        MovingAverageResult: remapped changes.
+    """
+    out = []
+    for change in [changes.start_STR, changes.end_STR, changes.shock]:
+        f = interp1d(changes.t, change, kind="linear", fill_value="extrapolate")  # type: ignore
+        out.append(f(original_t))
+
+    remapped = MovingAverageResult(original_t, *out)
+    return remapped
 
 
 def main():
@@ -233,7 +365,10 @@ def main():
     path_shock_changes = Path("scripts/shock_changes.csv")
     df_shock_changes = read_shock_changes_csv(path_shock_changes)
     # plot_linear_regressions(linear_regressions(df_shock_changes), df_shock_changes)
-    moving_avg(df_shock_changes)
+    mov_avg = moving_avg(df_shock_changes)
+    remapped = remap_to_original_t(np.array(df_shock_changes.index), mov_avg)
+    plot_mov_avg(remapped)
+    remapped.save(Path("scripts/STR_locations.csv"))
 
 
 if __name__ == "__main__":
