75bf6b01fe63cc0310351ad8c22f514724191eb2
James Plank <plankjames@gmail.com>
2023-10-26 17:25:39 +0100

    fix: Imports

--- tests/test_analysis/test_loading.py
+++ tests/test_analysis/test_loading.py
@@ -2,10 +2,10 @@ from os import environ
 
 from dotenv import load_dotenv
 
-from hybrid_jp.analysis.loading import load_async
+from hybrid_jp.analysis.loading import load_sdfs_para
 
 
 def test_load_async():
     load_dotenv()
     test_data_dir = environ["TEST_DATA_DIR"]
-    load_async(test_data_dir, stop=2)
+    load_sdfs_para(test_data_dir, stop=2)
