040b96138c18271424ba267dc5ad09491cf32696
James Plank <plankjames@gmail.com>
2023-10-26 17:27:24 +0100

    feat: correlation length in b perp and para

--- scripts/correlation_len.py
+++ scripts/correlation_len.py
@@ -1,56 +1,40 @@
 # %%
+from functools import partial
 from multiprocessing import set_start_method
+from os import environ
 
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
-from dotenv import dotenv_values
-from phdhelper.colours import sim as colour
-from phdhelper.mpl import format
-from scipy.signal import correlate, correlation_lags
+from matplotlib.axes import Axes
+from phdhelper import mpl
+from phdhelper.colours import sim as colours
+from scipy.signal import correlate, correlation_lags  # type: ignore
 
 import hybrid_jp as hj
 import hybrid_jp.analysis as hja
 
 # %%
 set_start_method("fork")
+mpl.format()
+
 # %%
 # Loading data
-env = dotenv_values("../.env")
-get_env = lambda s, typ: typ(env[s])
-DATA_DIR = get_env("DATA_DIR", str)
-SUBDIVISIONS = get_env("SUBDIVISIONS", int)
-N_CHUNKS = get_env("N_CHUNKS", int)
-N_THREADS = get_env("N_THREADS", int)
-START_SDF = get_env("START_SDF", int)
-END_SDF = get_env("END_SDF", int)
-deck = hja.load_deck(DATA_DIR)
+cfg = hj.config_from_toml(environ["TOML_PATH"]).shk
+
+deck = hja.load_deck(cfg.data_dir)
 SDFs, fpaths = hja.load_sdfs_para(
-    DATA_DIR,
+    cfg.data_dir,
     dt=deck.output.dt_snapshot,
-    threads=N_THREADS,
-    start=START_SDF,
-    stop=END_SDF,
+    threads=cfg.n_threads,
+    start=cfg.start_sdf,
+    stop=cfg.stop_sdf,
 )
 for SDF in SDFs:
     SDF.mag *= 1e9  # nT
     SDF.mid_grid *= 1e-3  # km
 cs = hja.CenteredShock(SDFs, deck)
-
-# %%
-# Average over y (not sure if this is ideal)
-bg_B = deck.constant.B0 * 1e9
-
-
-def get_mean_b(sdf: hj.sdf_files.SDF):
-    return np.linalg.norm(np.stack([i for i in sdf.mag], axis=0), axis=0).mean(axis=1)
-
-
-cs.n_chunks = N_CHUNKS
-chnk = cs.downstream_start_chunk
-t = int(np.argmax(cs.valid_chunks[chnk]))
-
-mag, _ = cs.get_qty_in_frame(get_mean_b, chnk, t)
+cs.n_chunks = cfg.n_chunks
 
 
 # %%
@@ -74,5 +58,75 @@ def corr_len_1d(arr: npt.NDArray, dx: float):
     return correlation_length
 
 
-clen = corr_len_1d(mag, cs.dx)
-print(f"Correlation length {clen / (deck.constant.di/1000):.2f}di")
+class MagneticField:
+    def __init__(self, cs: hja.CenteredShock) -> None:
+        self.cs = cs
+        self.chunk_i = self.cs.chunk_i
+        self.ny = self.cs.grid.y.size
+
+    @staticmethod
+    def get_mag_xyz(sdf: hj.sdf_files.SDF, q: str) -> hj.arrfloat:
+        return getattr(sdf.mag, q)
+
+    f_x = partial(get_mag_xyz, q="bx")
+    f_y = partial(get_mag_xyz, q="by")
+    f_z = partial(get_mag_xyz, q="bz")
+    funcs = [f_x, f_y, f_z]
+
+    def b_xyz_frame(
+        self,
+        chunk: int,
+        t_idx: int,
+    ) -> hj.arrfloat:
+        out = np.empty((self.chunk_i, self.ny, 3), dtype=np.float64)
+        for i, f in enumerate(self.funcs):
+            out[:, :, i], _ = self.cs.get_qty_in_frame(
+                qty_func=f, chunk=chunk, t_idx=t_idx
+            )
+        return out
+
+    def b_xyz_chunk(self, chunk: int) -> hj.arrfloat:
+        times = np.nonzero(self.cs.valid_chunks[chunk])[0]
+        out = np.empty((times.size, self.chunk_i, self.ny, 3), dtype=np.float64)
+        for i, time in enumerate(times):
+            out[i] = self.b_xyz_frame(chunk=chunk, t_idx=time)
+
+        return out
+
+
+def b_perp_para_chunk(chunk: int, mag: MagneticField) -> hj.arrfloat:
+    b_t_xyz = mag.b_xyz_chunk(chunk)
+    b_xyz = b_t_xyz.mean(axis=0)
+
+    mean_b = b_xyz.mean(axis=(0, 1))
+    unit_b = mean_b / np.linalg.norm(mean_b)
+
+    b_basis = hj.arrays.create_orthonormal_basis_from_vec(unit_b)
+    b_rotated = hj.arrays.rotate_arr_to_new_basis(b_basis, b_xyz)
+
+    out = np.empty((b_xyz.shape[0], b_xyz.shape[1], 2), dtype=np.float64)
+    out[:, :, 0] = b_rotated[:, :, 0]
+    out[:, :, 1] = np.linalg.norm(b_rotated[:, :, 1:], axis=2)
+
+    return out
+
+
+# %%
+
+mag = MagneticField(cs)
+x = np.arange(cs.chunk_i * cs.n_chunks, dtype=np.float64) - (
+    cs.chunk_i * cs.downstream_start_chunk
+)
+x *= cs.dx
+dat = np.empty((x.size, 2))
+for c in range(cs.n_chunks):
+    c_idx = c * cs.chunk_i
+    dat[c_idx : c_idx + cs.chunk_i] = b_perp_para_chunk(c, mag).mean(axis=1)
+
+fig, ax = plt.subplots()
+
+ax.plot(x, dat[:, 0], c=colours.green(), label=r"$B_\parallel$")
+ax.plot(x, dat[:, 1], c=colours.red(), label=r"$B_\perp$")
+ax.legend()
+fig.tight_layout()
+plt.show()
