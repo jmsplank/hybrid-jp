7629dc96b314b75e39e3f3b8f5eca216ed9fe4bf
James Plank <plankjames@gmail.com>
2023-11-14 11:16:29 +0000

    feat: 1d corr using idx_1

--- scripts/correlation_2d.py
+++ scripts/correlation_2d.py
@@ -1,7 +1,8 @@
 # %%
+import re
 from multiprocessing import set_start_method
 from os import environ
-from turtle import color
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,11 +10,21 @@ from matplotlib.axes import Axes
 from matplotlib.collections import QuadMesh
 from matplotlib.figure import Figure
 from phdhelper import mpl
+from phdhelper.colours import sim as colours
 from scipy.signal import correlate, correlate2d, correlation_lags  # type: ignore
 
 import hybrid_jp as hj
 from hybrid_jp import analysis as hja
 from hybrid_jp.analysis.mag import MagneticField
+
+
+def make_fname_safe(name: str):
+    s = str(name).strip().replace(" ", "_")
+    s = re.sub(r"(?u)[^-\w.]", "", s)
+    if s in {"", ".", ".."}:
+        raise Exception("Could not derive file name from '%s'" % name)
+    return s
+
 
 # %%
 mpl.format()
@@ -34,9 +45,9 @@ cs.n_chunks = cfg.n_chunks
 field_getter = MagneticField(cs)
 
 # %%
-chunk = cs.downstream_start_chunk
+chunk = 18  # cs.downstream_start_chunk
 test_b = field_getter.b_xyz_frame(
-    cs.downstream_start_chunk,
+    chunk,
     int(np.argmax(cs.valid_chunks[chunk])),
 )
 
@@ -85,11 +96,11 @@ basis = hj.arrays.create_orthonormal_basis_from_vec(vec, e2_plane=e2_plane)
 
 rot_b = hj.arrays.rotate_arr_to_new_basis(basis, test_b)
 
-fig, ax = plt.subplots()
-im = plot_strem(ax, rot_b[:, :, (1, 2, 0)], x, cs.grid.y)
-fig.colorbar(im, ax=ax, label=r"$B_{\parallel}$")
-fig.tight_layout()
-plt.show()
+# fig, ax = plt.subplots()
+# im = plot_strem(ax, rot_b[:, :, (1, 2, 0)], x, cs.grid.y)
+# fig.colorbar(im, ax=ax, label=r"$B_{\parallel}$")
+# fig.tight_layout()
+# plt.show()
 
 
 # %%
@@ -128,6 +139,7 @@ im = ax.pcolormesh(
     cmap=mpl.Cmaps.diverging,
     vmax=vmax,
     vmin=-vmax,
+    rasterized=True,
 )
 cl = ax.contour(lags_x, lags_y, corr2d.T, colors="k", levels=np.arange(10) / 10)
 ax.contourf(
@@ -155,11 +167,13 @@ ax.set_ylim(lags_y[0], lags_y[-1])
 fig.colorbar(cl, cax=cax, label="Correlation Length [$d_i$]")
 fig.colorbar(im, cax=cax)
 fig.tight_layout()
+plt.savefig(f"correlation_2d/2D_autocorr_{make_fname_safe(comp[use_c])}.pdf")
 plt.show()
 
 
 # %%
-def corr_len_1d(arr: hj.arrfloat, dx: float) -> float:
+def corr_1d(arr: hj.arrfloat, dx: float) -> tuple[hj.arrfloat, hj.arrfloat]:
+    """Correlate 1D signal."""
     arr -= arr.mean()
     lags = correlation_lags(arr.size, arr.size, mode="full")
     correlated = correlate(arr, arr, mode="full")
@@ -168,6 +182,11 @@ def corr_len_1d(arr: hj.arrfloat, dx: float) -> float:
     lags = lags[lags >= 0]
     lags = lags * dx
 
+    return lags, correlated
+
+
+def corr_len_1d(lags: hj.arrfloat, correlated: hj.arrfloat) -> float:
+    """Obtain correlation length using integration method."""
     try:
         zero_idx = np.nonzero(correlated <= 0)[0][0]
         corr_upto0 = correlated[:zero_idx]
@@ -181,11 +200,114 @@ def corr_len_1d(arr: hj.arrfloat, dx: float) -> float:
 
 
 def corr_len_2d(arr: hj.arrfloat, dx: float, dy: float) -> tuple[float, float]:
-    cx = corr_len_1d(arr[:, 0], dx)
-    cy = corr_len_1d(arr[0, :], dy)
+    cx = corr_len_1d(*corr_1d(arr[:, 0], dx))
+    cy = corr_len_1d(*corr_1d(arr[0, :], dy))
     return cx, cy
 
 
-print(corr_len_2d(test_b[:, :, 0], cs.dx, cs.dy))
+def make_plot_1d_corr(
+    arr: hj.arrfloat,
+    delta: float,
+    title: str,
+    component_name="i",
+    save: bool = False,
+):
+    """_summary_
 
-# %%
+    Args:
+        arr (hj.arrfloat): 1D array to be autocorrelated
+        delta (float): spacing between points in `arr`
+        title (str): the title of the plot
+        component_name (str, optional): subs to B_{component_name}. Defaults to "i".
+        save (bool, optional): Save the image. Defaults to False.
+    """
+    l, c = corr_1d(arr, delta)
+    l0 = np.nonzero(c <= 0)[0][0]
+    l_C = corr_len_1d(l, c)
+
+    ax: Axes
+    fig, ax = plt.subplots()
+
+    ax.plot(l, c)
+    ax.scatter(l, c, marker=".", color=colours.dark(), s=10)
+    ax.scatter(
+        l[l0],
+        0,
+        marker="x",
+        color="red",
+        label=f"Zero crossing at ${l[l0]:.2f}d_i$",
+    )
+    ax.axhline(0, ls="--")
+    ax.fill_between(
+        l[: l0 + 1],
+        0,
+        c[: l0 + 1],
+        color="red",
+        alpha=0.1,
+        label=rf"$\lambda_C = {l_C:0.2f}$ [$d_i$]",
+    )
+
+    inset = ax.inset_axes((0.5, 0.5, 0.47, 0.47))
+    inset.plot(
+        np.arange(arr.size)[l0:] * delta,
+        (arr - arr.mean())[:-l0],
+        color=colours.red(),
+        ls="--",
+        alpha=0.5,
+        label=rf"$B_{component_name}(x\rightarrow x+{l[l0]:.2f}d_i)$",
+    )
+    inset.plot(np.arange(arr.size) * delta, arr - arr.mean())
+    inset.set_xlabel("$x$ [$d_i$]")
+    inset.set_ylabel(rf"$B_{component_name} - \left<B_{component_name}\right>$ [$nT$]")
+    inset.grid(False)
+    inset.legend()
+
+    ax.legend(loc="upper left")
+    ax.set_title(title)
+    ax.grid(False)
+    ax.set_ylabel(r"Autocorrelation")
+    ax.set_xlabel(r"Lag $\ell$ [$d_i$]")
+
+    fig.tight_layout()
+
+    if save:
+        file_dir = Path(__file__)
+        save_dir = file_dir.parent / file_dir.stem
+        save_dir.mkdir(exist_ok=True)
+        save_name = save_dir / (make_fname_safe(title) + ".png")
+        plt.savefig(save_name)
+
+    plt.show()
+
+
+make_plot_1d_corr(
+    test_b[:, 0, 0],
+    cs.dx,
+    r"1D $B_\parallel$ correlation along $x$",
+    component_name=r"\parallel",
+    save=True,
+)
+
+make_plot_1d_corr(
+    test_b[0, :, 0],
+    cs.dy,
+    r"1D $B_\parallel$ correlation along $y$",
+    component_name=r"\parallel",
+    save=True,
+)
+
+make_plot_1d_corr(
+    test_b[:, 0, 1],
+    cs.dy,
+    r"1D $B_{\perp i}$ correlation along $x$",
+    component_name=r"{\perp i}",
+    save=True,
+)
+
+make_plot_1d_corr(
+    test_b[0, :, 1],
+    cs.dy,
+    r"1D $B_{\perp i}$ correlation along $y$",
+    component_name=r"{\perp i}",
+    save=True,
+)
